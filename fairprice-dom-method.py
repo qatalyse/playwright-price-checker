14fd2baa4b221087a41af1ea124a2dd44b9884f9
qatalyse <qatalyse@gmail.com>
2026-06-18 05:01:26 +0800

    verify if github actions has write access

--- fairprice-dom-method.py
+++ fairprice-dom-method.py
@@ -6,7 +6,7 @@ urls = [
     # "https://www.fairprice.com.sg/product/holland-potato-china-1kg-13057650",
     # "https://www.fairprice.com.sg/product/buttercup-luxury-spread-250g-366329",
     # "https://www.fairprice.com.sg/product/pasar-fresh-eggs-10-eggs-550g-451724",
-    # "https://www.fairprice.com.sg/product/campbells-soup-mushroom-potage-305g-465098",
+    "https://www.fairprice.com.sg/product/campbells-soup-mushroom-potage-305g-465098",
     "https://www.fairprice.com.sg/product/myojo-instant-noodles-ramen-char-mee-5s-x-75g-88577",
     "https://www.fairprice.com.sg/product/myojo-instant-noodles-mee-goreng-original-5s-x-80g-10647588",
     # "https://www.fairprice.com.sg/product/indomie-mi-goreng-instant-noodles-special-5-x-85g-13057731"
